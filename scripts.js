2cb905679795f4523f26a3ef897b6359dd740fba
ky7510 <yumakim70@gmail.com>
2024-08-23 21:58:04 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// scripts.js
+
+document.querySelectorAll('.content').forEach(section => {
+    section.addEventListener('click', () => {
+        const sectionTitle = section.querySelector('h2').innerText;
+        alert(`استكشاف المزيد حول: ${sectionTitle}`);
+    });
+});
+document.querySelectorAll('video').forEach(video => {
+    video.addEventListener('click', () => {
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    });
+});
